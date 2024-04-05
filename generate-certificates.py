ccf99fd96d2cea7865c2e7b8491c0012e4234f1f
Leonardo Garcia <leonardo.garcia@linaro.org>
2024-04-05 10:30:29 -0300

    Create certification of participation for speakers as well.

--- generate-certificates.py
+++ generate-certificates.py
@@ -15,35 +15,54 @@
 import os
 from datetime import datetime
 
-# Read LaTeX template for certificates of participation
-# This is a one-time operation, no need to read the template for every name
+# Read LaTeX template for certificates of participation of attendees
+# This is a one-time operation, no need to read the template for every attendee
 with open("template-attendee.tex", "r") as f:
     latex_template_attendee = f.read()
 
-def generate_pdf(name):
+# Read LaTeX template for certificates of participation of speakers
+# This is a one-time operation, no need to read the template for every speaker
+with open("template-speaker.tex", "r") as f:
+    latex_template_speaker = f.read()
+
+
+def generate_pdf(name, talk=None):
     """
-    Generate a PDF certificate for the given name.
+    Generate a PDF certificate for the given name and optional talk.
 
     Parameters:
     name (str): The name of the attendee
+    talk (str): The name of a talk the attendee presented (optional)
 
     """
-    # Replace the placeholder in the template with the actual name
-    latex_code = latex_template_attendee.replace("[Name of the Attendee]", name)
-    
+    if (talk is None):
+        # Replace the placeholder in the attendee template with the actual name
+        latex_code = latex_template_attendee.replace(
+            "[Name of the Attendee]", name)
+        destination = "attendees"
+    else:
+        # Replace the placeholders in the speaker template with the actual name and talk
+        latex_code = latex_template_speaker.replace(
+            "[Name of the Speaker]", name)
+        latex_code = latex_code.replace("[Name of the Talk]", talk)
+        destination = "speakers"
+
     # Write the modified LaTeX code to a temporary .tex file
     with open("temp.tex", "w") as f:
         f.write(latex_code)
-        
+
     # Compile the LaTeX code to produce a PDF
     # NOTE: You must have pdflatex installed and available in the PATH
     os.system("pdflatex temp.tex")
 
     # Generate a timestamp for unique file naming
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    
+
     # Rename the generated PDF file to include the attendee name and timestamp
-    os.rename("temp.pdf", f"{name}_certificate_{timestamp}.pdf")
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    os.rename("temp.pdf", f"{destination}/{name}_certificate_{timestamp}.pdf")
+
 
 # Read attendee's names from a file
 # Assumes one name per line in the file
@@ -52,8 +71,16 @@ with open("attendees.txt", "r") as f:
 
 # Loop through each name and generate a PDF certificate
 for attendee in attendees:
-    attendee = attendee.strip()  # Remove any leading/trailing whitespace or newline characters
+    # Remove any leading/trailing whitespace or newline characters
+    attendee = attendee.strip()
     generate_pdf(attendee)  # Call the function to generate PDF
+
+# Read talks and speakers informatoin from a file
+# Assumes talk title in the first line, followed by speaker's names in subsequent lines until an empty line is found
+with open("speakers.txt", "r") as f:
+    while (talk := f.readline().strip()):
+        while (speaker := f.readline().strip()):
+            generate_pdf(speaker, talk)
 
 # Cleanup: Remove temporary files generated during PDF creation
 os.remove("temp.tex")
